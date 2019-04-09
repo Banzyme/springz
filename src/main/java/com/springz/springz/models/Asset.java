b226a558131d2d8715ecf4ddf739440e3e91d970
Ndamulelo Nemakhavhani <nbanzyme@gmail.com>
2019-04-09 13:06:28 +0200

    Create initial entity
    Signed-off-by: N Nemakhavhani <ndamulelo@bbd.co.za>

---
+++ src/main/java/com/springz/springz/models/Asset.java
@@ -0,0 +1,55 @@
+package com.springz.springz.models;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+
+@Entity
+public class Asset {
+    @Id
+    @GeneratedValue( strategy = GenerationType.AUTO)
+    private Integer ID;
+    @NotEmpty
+    @NotNull
+    private  String name;
+    private String description;
+    @NotEmpty
+    @NotNull
+    private  String state;
+
+    public Integer getID() {
+        return ID;
+    }
+
+    public void setID(Integer ID) {
+        this.ID = ID;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+}
